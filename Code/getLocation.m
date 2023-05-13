493112c13e9bf967e1aebf74d50fd154b206f678
xinjie0yang <108706394+xinjie0yang@users.noreply.github.com>
2023-05-13 21:44:00 +0800

    Delete Code directory

--- Code/getLocation.m
+++
@@ -1,6 +0,0 @@
-function Location = getLocation(Dec,V,w,lower)
-% find the corresponding subgrid from Grid, according to decs of solution
-    [N,~] = size(Dec);
-    Loc = floor((Dec-repmat(lower,N,1))./repmat(w,N,1))+1;
-    Location = max(1,min(Loc,repmat(V,N,1)));
-end
